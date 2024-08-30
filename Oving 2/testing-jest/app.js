2558774a841a2bb7b2be5624302ecba89e08bd86
sonson-arch <sondrhso@stud.ntnu.no>
2024-08-30 10:18:22 +0200

    #

--- Oving 2/testing-jest/app.js
+++ Oving 2/testing-jest/app.js
@@ -1,5 +1,6 @@
 export function isLeapYear(year) {
-    return (year % 4 === 0) &&
-           (year % 100 !==  0) ||
-           (year % 400 === 0);
+    if (year < 0 || year === null || year === undefined) {
+        throw new Error('Invalid argument');
+    }
+    else return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0) 
 }
--- Oving 2/testing-jest/app.test.js
+++ Oving 2/testing-jest/app.test.js
@@ -1,12 +1,49 @@
 import {isLeapYear} from './app.js';
 
+// Oppgave 1
 
-describe('A year is not a leap year', () => {
-    test('Year is not divisible by 4', () => {
-        expect(isLeapYear(1981)).toBe(false);
-    });
-    
-    test('Year is divisible by 100 but not by 400', () => {
-        expect(isLeapYear(2100)).toBe(false);
+describe('A year is a leap year', () => {
+    test.each([
+        [1820, true],
+        [1960, true],
+        [2020, true]
+    ])('Year %i is a leap year', (year, expected) => {
+        expect(isLeapYear(year)).toBe(expected);
     });
 });
+ 
+// Oppgave 2 & 3 & 4 
+
+describe('A year is not supported', () => {
+    test.each([
+        [-1, false]
+    ])('Year %i is not supported', (year) => {
+        expect(() => isLeapYear(year)).toThrow('Invalid argument');
+    })
+});
+
+describe('A year is undefined', () => {
+    test('Year is undefined', () => {
+        expect(() => isLeapYear()).toThrow('Invalid argument');
+    })
+})
+
+describe('A year is null', () => {
+    test('Year is null', () => {
+        expect(() => isLeapYear(null)).toThrow('Invalid argument');
+    })
+})
+
+describe('A year is a leap year', () => {
+    test.each([
+        [1820, true],
+        [1960, true],
+        [2020, true],
+        [1900, false], 
+        [2000, true],    
+    ])('Year %i is a leap year', (year, expected) => {
+            expect(isLeapYear(year)).toBe(expected);
+
+    });
+})
+
